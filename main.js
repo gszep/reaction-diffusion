3ac4d51ac9bbc8b7a953c4e6ffb2a927b8b788de
Gregg <gregory.szep@gmail.com>
2018-02-05 22:22:47 +0000

    factored out three.js library

--- main.js
+++ main.js
@@ -4,106 +4,52 @@ Author: Gregory Szep, King's College London 2017
 Adapted from code by Pablo Márquez Neila
 */
 
-/* global gl:true */
+/* global gl:true $*/
 
-var FBO, FBO2, texture, texture2, it = 1,
-	a = .8, b = .01, dt = .05, eps = .02, h = .607
+// rendering global
+var renderStep = 20
 
-
+// main simulation canvas
 function Simulation(canvas,coordinate,integrator,painter) {
+	gl = this.getWebGL(canvas)
 
-	gl = document.getElementById(canvas).getContext('webgl2')
-	if (!window.WebGLRenderingContext || !gl ){
-		alert('Your browser does not support WebGL 2.0.'); return}
+	// compile glsl shaders and initialise planar mesh
+	this.compileShaders(coordinate,integrator,painter)
+	this.nComponents = this.getComponents(integrator)
+	this.setGeometry()
 
-	this.integrator  = gl.createProgram()
-	gl.attachShader(this.integrator, this.getShader(coordinate,'vert'))
-	gl.attachShader(this.integrator, this.getShader(integrator,'frag'))
-	gl.linkProgram(this.integrator)
-	this.painter  = gl.createProgram()
-	gl.attachShader(this.painter, this.getShader(coordinate,'vert'))
-	gl.attachShader(this.painter, this.getShader(painter,'frag'))
-	gl.linkProgram(this.painter)
+	// initialise parameters and interactions
+	this.setParameters()
+	this.sliders()
+	this.mouseEvents()
 
-	var posBuffer = gl.createBuffer()
-	gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer)
-	var vertices = new Float32Array([-1,-1,0, 1,-1,0, -1,1,0, 1,1,0])
-	var aPosLoc = gl.getAttribLocation(this.integrator, 'position')
-	gl.enableVertexAttribArray( aPosLoc )
-	var aTexLoc = gl.getAttribLocation(this.integrator, 'aTexCoord')
-	gl.enableVertexAttribArray( aTexLoc )
-	var texCoords = new Float32Array([0,0, 1,0, 0,1, 1,1])
-	var texCoordOffset = vertices.byteLength
-	gl.bufferData(gl.ARRAY_BUFFER,
-		texCoordOffset + texCoords.byteLength, gl.STATIC_DRAW)
-	gl.bufferSubData(gl.ARRAY_BUFFER, 0, vertices)
-	gl.bufferSubData(gl.ARRAY_BUFFER, texCoordOffset, texCoords)
-	gl.vertexAttribPointer(aPosLoc, 3, gl.FLOAT, gl.FALSE, 0, 0)
-	gl.vertexAttribPointer(aTexLoc, 2, gl.FLOAT, gl.FALSE, 0, texCoordOffset)
+	// initial condition
+	this.pixels = this.initialCondition()
+	this.setBuffer(this.pixels)
 
-	texture = gl.createTexture()
-	gl.bindTexture(gl.TEXTURE_2D, texture)
-	gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1)
-	var pixels = [], tSize = 512
-	for(var i = 0; i<tSize; i++)
-		for(var j = 0; j<tSize; j++){
-			if (Math.random() < .001) pixels.push( 250 )
-			else pixels.push( 0 )
-			if (Math.random() < .5) pixels.push( 250 )
-			else pixels.push( 0 )
-			pixels.push( 0 )
-			pixels.push( 0 )
-		}
-
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, tSize, tSize, 0,
-		gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(pixels))
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	texture2 = gl.createTexture()
-	gl.bindTexture(gl.TEXTURE_2D, texture2)
-	gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1)
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, tSize, tSize, 0,
-		gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(pixels))
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	FBO = gl.createFramebuffer()
-	gl.bindFramebuffer(gl.FRAMEBUFFER, FBO)
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
-		gl.TEXTURE_2D, texture, 0)
-	FBO2 = gl.createFramebuffer()
-	gl.bindFramebuffer(gl.FRAMEBUFFER, FBO2)
-	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
-		gl.TEXTURE_2D, texture2, 0)
-
-	gl.useProgram(this.integrator)
-	gl.uniform1f(gl.getUniformLocation(this.integrator, 'a'), a)
-	gl.uniform1f(gl.getUniformLocation(this.integrator, 'ba'),  b/a)
-	gl.uniform1f(gl.getUniformLocation(this.integrator, 'dt'), dt)
-	gl.uniform1f(gl.getUniformLocation(this.integrator, 'dte'), dt/eps)
-	gl.uniform1f(gl.getUniformLocation(this.integrator, 'dth2'), dt/(h*h))
-	gl.uniform1i(gl.getUniformLocation(this.integrator, 'uTexSamp'), 0)
-	gl.useProgram(this.painter)
-	gl.uniform1i(gl.getUniformLocation(this.painter, 'uTexSamp'), 0)
-
-
+	// begin simulation
 	this.renderLoop()
 }
 
 
-Simulation.prototype.render = function() {
-	gl.useProgram(this.integrator)
-	if (it > 0){
-		gl.bindTexture(gl.TEXTURE_2D, texture)
-		gl.bindFramebuffer(gl.FRAMEBUFFER, FBO2)}
-	else{
-		gl.bindTexture(gl.TEXTURE_2D, texture2)
-		gl.bindFramebuffer(gl.FRAMEBUFFER, FBO)}
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
-	gl.flush()
+// setting initial condition
+Simulation.prototype.initialCondition = function() {
+	var allpixels = []
+	for ( let n = 0; n < this.nComponents; n++ ) {
+		let pixels = []
 
-	it = -it
-
-	this.display()
+		for(var i = 0; i<this.width; i++)
+			for(var j = 0; j<this.height; j++){
+				if (Math.random() < .001) pixels.push( 0 )
+				else pixels.push( 0 )
+				if (Math.random() < .5) pixels.push( 0 )
+				else pixels.push( 0 )
+				pixels.push( 0 )
+				pixels.push( 0 )
+			}
+		allpixels.push(pixels)
+	}
+	return allpixels
 }
 
 
@@ -114,12 +60,394 @@ Simulation.prototype.renderLoop = function() {
 }
 
 
-// display materials with component to color map
-Simulation.prototype.display = function() {
+// single animation iteration
+Simulation.prototype.render = function() {
 
+	// update parameters
+	this.updateParameters()
+
+	// propagate for a given number of steps
+	this.propagate()
+
+	// render texture to canvas
+	this.display()
+}
+
+
+// propagate textures for given number of timesteps
+Simulation.prototype.propagate = function() {
+	gl.useProgram(this.integrator)
+
+	// alternate two targets in buffer per step
+	renderStep = renderStep % 2 == 0 ? renderStep : renderStep+1
+	for ( var i = 0; i < renderStep; ++i ) {
+
+		// compute step, render to next textures in buffer
+		gl.bindFramebuffer(gl.FRAMEBUFFER, this.buffer[i%2])
+		this.bindComponents(this.integrator,i+1)
+		gl.drawArrays(gl.TRIANGLE_STRIP,0,4)
+
+		// remove brush from canvas
+		this.resetBrush()
+	}
+}
+
+
+// initialise parameters from specifications
+Simulation.prototype.setParameters = function() {
+	this.parameters = {
+		'spaceStep': 1.0, 'timeStep': 0.001,
+		'brush': [-1,-1,0,0], 'colors': [],
+		'diffusionRatio': 1.0,
+
+		'a': .8,  'ba': .01/.8
+	}
+}
+
+
+// TODO(@gszep) factor paramter properties
+Simulation.prototype.sliders = function() {
+	var that = this
+
+	$('#diffusionRatioSlider').slider({
+		value: that.parameters.diffusionRatio, min: 0, max:2, step:0.001,
+
+		change: function(event, ui) {
+			$('#diffusionRatio').html(ui.value)
+			that.parameters.diffusionRatio = ui.value
+		},
+
+		slide: function(event, ui) {
+			$('#diffusionRatio').html(ui.value)
+			that.parameters.diffusionRatio = ui.value
+		}
+	})
+	$('#diffusionRatioSlider').slider('value', that.parameters.diffusionRatio)
+
+	$('#timeStepSlider').slider({
+		value: that.parameters.timeStep, min: 0.001, max:0.21, step:0.001,
+
+		change: function(event, ui) {
+			$('#timeStep').html(ui.value)
+			that.parameters.timeStep = ui.value
+		},
+
+		slide: function(event, ui) {
+			$('#timeStep').html(ui.value)
+			that.parameters.timeStep = ui.value
+		}
+	})
+	$('#timeStepSlider').slider('value', that.parameters.timeStep)
+
+	$('#spaceStepSlider').slider({
+		value: that.parameters.spaceStep, min:0.1 , max:10, step:0.1,
+
+		change: function(event, ui) {
+			$('#spaceStep').html(ui.value)
+			that.parameters.spaceStep = ui.value
+		},
+
+		slide: function(event, ui) {
+			$('#spaceStep').html(ui.value)
+			that.parameters.spaceStep = ui.value
+		}
+	})
+	$('#spaceStepSlider').slider('value', that.parameters.spaceStep)
+}
+
+
+// mouse events and color gradients
+Simulation.prototype.mouseEvents = function() {
+	var that = this
+
+	// TODO(@gszep) factor brush properties into json/method
+	this.brush = { radius: 0.1 }
+
+
+	this.canvas.onmouseup = function() {
+		that.isMouseDown = false
+	}
+
+	this.canvas.onmousedown = function(event) {
+		that.isMouseDown = true
+		var component
+
+		if (event.which == 1)
+			component = 0
+
+		if (event.which == 2)
+			component = 1
+
+		if (event.which == 3)
+			component = 2
+
+		that.parameters.brush = [
+			this.mouseX/that.width,
+			1-this.mouseY/that.height,
+			that.brush.radius,component]
+	}
+
+	this.canvas.onmousemove = function(event) {
+		var mouseEvent = event ? event : window.event
+		var component
+
+		this.mouseX = mouseEvent.pageX - $('#'+that.canvas.id).offset().left
+		this.mouseY = mouseEvent.pageY - $('#'+that.canvas.id).offset().top
+
+		if (event.which == 1)
+			component = 0
+
+		if (event.which == 2)
+			component = 1
+
+		if (event.which == 3)
+			component = 2
+
+		if(that.isMouseDown){
+			that.parameters.brush = [
+				this.mouseX/that.width,
+				1-this.mouseY/that.height,
+				that.brush.radius,component]
+		}
+	}
+
+	// use mousewheel to change brush radius
+	this.canvas.onmousewheel = function(event) {
+		var delta = event.wheelDelta ? event.wheelDelta : -event.detail
+		var radius = that.brush.radius + delta/10000
+
+		that.brush.radius = radius > 0 ? radius : 0.01
+	}
+
+	// TODO(@gszep) use number keys for selecting components
+	window.onkeydown = function(event) {
+		event.preventDefault()
+
+		if ( event.code == 'Enter' )
+			that.pixels = that.getPixels()
+		if ( event.code == 'Space' )
+			that.setBuffer(that.pixels)
+	}
+
+	// bind gradient widget to colors in painter
+	$('#gradient').gradient('setUpdateCallback',function () { that.updateColors() })
+	this.updateColors()
+
+	// prevent context menue from opening on right-click
+	this.canvas.oncontextmenu = function (event) { event.preventDefault() }
+}
+
+
+// pass updated parameter values to shaders
+Simulation.prototype.updateParameters = function() {
+
+	// itegrate through shaders
+	['this.integrator','this.painter'].forEach( program => {
+		gl.useProgram(eval(program))
+
+		// iterate through parameter list
+		for (let key in this.parameters){
+			let parameter = this.parameters[key]
+
+			// pass in scalars
+			if (typeof parameter === 'number')
+				eval('gl.uniform1f(gl.getUniformLocation('+
+				program+', key), parameter)')
+
+			if (Array.isArray(parameter)) {
+				let value = parameter[0]
+
+				// pass in vectors
+				if (typeof value === 'number')
+					eval('gl.uniform'+parameter.length+
+						'fv(gl.getUniformLocation('+
+						program+', key), parameter)')
+
+				// pass in matricies
+				if (Array.isArray(value))
+					for ( let i = 0; i < parameter.length; i++ )
+						eval('gl.uniform'+value.length+
+							'fv(gl.getUniformLocation('+program+', key+"['+i+']"), parameter['+i+'])')
+			}
+		}
+	})
+}
+
+
+// update color gradient used to paint results
+Simulation.prototype.updateColors = function() {
+	var colors = $('#gradient').gradient('getValuesRGBS')
+
+	for( let i=0; i<colors.length; i++) {
+		this.parameters.colors[i] = colors[i]
+	}
+}
+
+
+// remove perturbation brush
+Simulation.prototype.resetBrush = function() {
+	this.parameters.brush = [-1,-1,0,0]
+}
+
+
+// initialise frame buffers with given component pixels[n]
+Simulation.prototype.setBuffer = function(pixels) {
+	this.buffer = []
+
+	// create two texture targets per component
+	this.setTextures(pixels)
+
+	// create two buffers for alternating frame method
+	for ( let i = 0; i < 2; i++ ) {
+
+		let buffer = gl.createFramebuffer()
+		gl.bindFramebuffer(gl.FRAMEBUFFER,buffer)
+
+		// bind component textures to frame buffers
+		for ( let n = 0; n < this.nComponents; n++ ) {
+			gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0+n,
+				gl.TEXTURE_2D, this.textures[i][n], 0)
+		}
+		this.buffer.push(buffer)
+	}
+
+	// bind component textures to painter program
+	gl.useProgram(this.painter)
+	this.bindComponents(this.painter,1)
+
+	// check for errors
+	let status = gl.checkFramebufferStatus(gl.FRAMEBUFFER)
+	if (status!=gl.FRAMEBUFFER_COMPLETE)
+		throw 'Incomplete Frame Buffer: '+status
+}
+
+
+// initialise textures with given pixels
+Simulation.prototype.setTextures = function(pixels) {
+	this.textures = []
+
+	// two targets for frame buffer
+	for ( let i = 0; i < 2; i++ ) {
+		let textures = []
+
+		// create one texture for each component
+		for ( let n = 0; n < this.nComponents; n++ ) {
+			gl.activeTexture(gl.TEXTURE0+n+i*this.nComponents)
+
+			let texture = gl.createTexture()
+			gl.bindTexture(gl.TEXTURE_2D, texture)
+
+			// texture properties
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+
+			// initialise texture pixels with data
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA32F, this.width, this.height, 0,
+				gl.RGBA, gl.FLOAT, new Float32Array(pixels[n]))
+
+			textures.push(texture)
+		}
+		this.textures.push(textures)
+	}
+}
+
+
+// set pointers from indexed buffer to uniforms in given program
+Simulation.prototype.bindComponents = function(program,bufferIndex) {
+	let attachments = []
+
+	for ( let n = 0; n < this.nComponents; n++ ) {
+		attachments.push(gl.COLOR_ATTACHMENT0+n)
+
+		gl.uniform1i(gl.getUniformLocation(program, 'component['+n+']'), n+(bufferIndex%2)*this.nComponents)
+	}
+
+	// set multiple render targets
+	gl.drawBuffers(attachments)
+}
+
+
+// get pixel data from components
+Simulation.prototype.getPixels = function() {
+
+	var width = Math.ceil(this.width)
+	var height = Math.ceil(this.height)
+	let data = []
+
+	// create temporary buffer to parse out data
+	let dataBuffer = gl.createFramebuffer()
+	gl.bindFramebuffer(gl.FRAMEBUFFER,dataBuffer)
+
+	// iterate through components
+	for ( let n = 0; n < this.nComponents; n++ ) {
+		let pixels = new Float32Array(4*width*height)
+
+		// bind texture to buffer
+		gl.framebufferTexture2D(
+			gl.FRAMEBUFFER,gl.COLOR_ATTACHMENT0,gl.TEXTURE_2D,this.textures[1][n],0)
+
+		// write texture pixels to image contexts
+		gl.readPixels(0,0,width,height,gl.RGBA,gl.FLOAT,pixels)
+		data.push(pixels)
+	}
+
+	gl.deleteFramebuffer(dataBuffer)
+	return data
+}
+
+
+// initialise mesh geomerty to render onto
+Simulation.prototype.setGeometry = function() {
+
+	let buffer = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+
+	// vertex position and texture surface limits
+	var xy = new Float32Array([-1,-1, 1,-1, -1,1, 1,1])
+	var uv = new Float32Array([0,0, 1,0, 0,1, 1,1])
+
+	// populate coordinate buffer
+	gl.bufferData(gl.ARRAY_BUFFER,xy.byteLength+uv.byteLength,gl.STATIC_DRAW)
+	gl.bufferSubData(gl.ARRAY_BUFFER,0,xy)
+	gl.bufferSubData(gl.ARRAY_BUFFER,xy.byteLength,uv)
+
+	// get location index of coordinates in shaders
+	let vertexIndex = gl.getAttribLocation(this.integrator, 'vertexCoordinate')
+	let textureIndex = gl.getAttribLocation(this.integrator, 'textureCoordinate')
+
+	// attach buffers to shaders
+	gl.vertexAttribPointer(vertexIndex,2,gl.FLOAT,gl.FALSE,0,0)
+	gl.vertexAttribPointer(textureIndex,2,gl.FLOAT,gl.FALSE,0,xy.byteLength)
+
+	gl.enableVertexAttribArray(vertexIndex)
+	gl.enableVertexAttribArray(textureIndex)
+}
+
+
+// show with component to color map
+Simulation.prototype.display = function() {
 	gl.useProgram(this.painter)
 	gl.bindFramebuffer(gl.FRAMEBUFFER,null)
 	gl.drawArrays(gl.TRIANGLE_STRIP,0,4)
+}
+
+
+// setup WebGL 2.0 environment
+Simulation.prototype.getWebGL = function(canvas) {
+	this.canvas = document.getElementById(canvas)
+	var gl = this.canvas.getContext('webgl2')
+
+	if (!window.WebGLRenderingContext || !gl ){
+		alert('WebGL 2.0 not supported'); return }
+
+	// import extensions
+	gl.getExtension('EXT_color_buffer_float')
+
+	// store dimensions
+	this.width = gl.drawingBufferHeight
+	this.height = gl.drawingBufferWidth
+
+	return gl
 }
 
 
@@ -127,9 +455,9 @@ Simulation.prototype.display = function() {
 Simulation.prototype.getShader = function(sourceCode,type) {
 	var shader
 
-	if (type=='vert')
+	if (type=='vertex')
 		shader = gl.createShader(gl.VERTEX_SHADER)
-	if (type=='frag')
+	if (type=='fragment')
 		shader = gl.createShader(gl.FRAGMENT_SHADER)
 
 	// compile shader
@@ -138,8 +466,47 @@ Simulation.prototype.getShader = function(sourceCode,type) {
 
 	// report any errors
 	if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)) {
-		console.error(gl.getShaderInfoLog(shader))
+		throw gl.getShaderInfoLog(shader)
 	}
 
 	return shader
+}
+
+
+// compile integrator and painter shaders from glsl code
+Simulation.prototype.compileShaders = function(coordinate,integrator,painter) {
+
+	let locate = this.getShader(coordinate,'vertex')
+	let integrate = this.getShader(integrator,'fragment')
+	let paint = this.getShader(painter,'fragment')
+
+	this.integrator  = gl.createProgram()
+	this.painter  = gl.createProgram()
+
+	gl.attachShader(this.integrator, locate)
+	gl.attachShader(this.painter, locate)
+
+	gl.attachShader(this.integrator, integrate)
+	gl.attachShader(this.painter, paint)
+
+	gl.linkProgram(this.integrator)
+	gl.linkProgram(this.painter)
+}
+
+
+// extract number of components from glsl code
+Simulation.prototype.getComponents = function(integrator) {
+
+	// use regex to extract number of components from code
+	var integratorRegex = integrator.match('NCOMPONENTS ([0-9])+')
+
+	// check if component keyword is present
+	if (integratorRegex) {
+
+		var integratorComponents = integratorRegex[0].match('([0-9])+')[0]
+		return integratorComponents
+	}
+	else {
+		throw 'no "#define NCOMPONENTS" declaration found in solver code'
+	}
 }
--- solver/integrator.frag
+++ solver/integrator.frag
@@ -1,46 +1,97 @@
 #version 300 es
+#define NCOMPONENTS 3
 
-#define GL2
-#ifdef GL_ES
-	precision highp float;
-#endif
+// use 16-bit preicision floats
+precision highp float;
 
-uniform sampler2D uTexSamp;
-uniform float dt;
-uniform float dte;
-uniform float dth2;
-uniform float a;
-uniform float ba;
-const float d = 1./512.;
+// components and temporal discretisation
+uniform sampler2D component[NCOMPONENTS];
+out vec4 outputComponent[NCOMPONENTS];
 
-in vec2 vTexCoord;
-out vec4 fragmentColor;
+uniform float spaceStep;
+uniform float timeStep;
 
-void main(void) {
-   vec4 t = texture(uTexSamp, vTexCoord);
-   float u = t.r,  v = t.g,  u2 = t.b,  v2 = t.a;
-   u += u2/256.;   v += v2/256.;
-   float vnew = v + (u - v)*dt,  uth = v/a + ba,  unew;
-   float tmp = dte*(u - uth);
-   if ( u <= uth)  unew = u/(1. - tmp*(1. - u));
-   else{
-      tmp *= u;
-      unew = (tmp + u)/(tmp + 1.);
-   }
-   unew += (texture(uTexSamp, vec2(vTexCoord.x, vTexCoord.y + d) ).r +
-      texture(uTexSamp, vec2(vTexCoord.x, vTexCoord.y - d) ).r +
-      texture(uTexSamp, vec2(vTexCoord.x + d, vTexCoord.y) ).r +
-      texture(uTexSamp, vec2(vTexCoord.x - d, vTexCoord.y) ).r +
+// user perturbation
+uniform vec4 brush;
 
-     (texture(uTexSamp, vec2(vTexCoord.x, vTexCoord.y + d) ).b +
-      texture(uTexSamp, vec2(vTexCoord.x, vTexCoord.y - d) ).b +
-      texture(uTexSamp, vec2(vTexCoord.x + d, vTexCoord.y) ).b +
-      texture(uTexSamp, vec2(vTexCoord.x - d, vTexCoord.y) ).b)/256.
+// input coordinate neighbourhood
+in vec2 centre;
+in vec2 left;
+in vec2 right;
+in vec2 top;
+in vec2 bottom;
 
-      - 4.*u)*dth2;
-   u2 = fract(unew*256.);
-   if (u2 > .5) unew -= d;
-   v2 = fract(vnew*256.);
-   if (v2 > .5) vnew -= d;
-   fragmentColor = vec4(unew, vnew, u2, v2 );
+in vec2 topleft;
+in vec2 topright;
+in vec2 bottomleft;
+in vec2 bottomright;
+
+// parameters
+uniform float diffusionRatio;
+
+// variables
+struct Coordinate {
+	vec4 point;
+	vec4 gradient;
+};
+
+
+// calculate discrete laplacian
+Coordinate laplacian(sampler2D sampler) {
+
+	// values of point
+	vec4 point = texture(sampler,fract(centre));
+
+	// construct point and gradient
+	Coordinate coordinate;
+	coordinate.point = point;
+	coordinate.gradient = (
+		4.0*texture(sampler,fract(left))+
+		4.0*texture(sampler,fract(right))+
+		4.0*texture(sampler,fract(bottom))+
+		4.0*texture(sampler,fract(top))+
+		1.0*texture(sampler,fract(topleft))+
+		1.0*texture(sampler,fract(topright))+
+		1.0*texture(sampler,fract(bottomleft))+
+		1.0*texture(sampler,fract(bottomright))-
+		20.0*point
+
+	) / 6.0;
+
+	return coordinate;
+}
+
+
+// this propagates the reaction-diffusion system
+void main() {
+
+	// calculate laplacians
+	Coordinate coordinate[NCOMPONENTS];
+	for( int i=0; i < NCOMPONENTS; i++ ) {
+		coordinate[i] = laplacian(component[i]);
+	}
+
+	// user perturbations
+	if(brush.x > 0.0) {
+
+		float location = distance(centre,brush.xy);
+		float radius = brush.z;
+		int componentIndex = int(brush.w);
+
+		//  within radius set all comonents to zero except chosen one
+		if( location < radius ) {
+			for (int i=0; i<NCOMPONENTS; i++) {
+
+				if (i == componentIndex)
+					coordinate[i].point.r = 1.0;
+				else
+					coordinate[i].point.r = 0.0;
+			}
+		}
+	}
+
+	// output components to buffer
+	outputComponent[0] = coordinate[0].point + timeStep*( coordinate[0].gradient + coordinate[1].point*coordinate[0].point - coordinate[0].point*coordinate[2].point );
+	outputComponent[1] = coordinate[1].point + timeStep*( coordinate[1].gradient - coordinate[1].point*coordinate[0].point - coordinate[1].point*coordinate[2].point + 2.0*coordinate[0].point*coordinate[2].point );
+	outputComponent[2] = coordinate[2].point + timeStep*( coordinate[2].gradient*diffusionRatio + coordinate[1].point*coordinate[2].point - coordinate[0].point*coordinate[2].point );
 }
