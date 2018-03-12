f0f5a7f325acb280aceebbf765feb8aea7e24c25
Gregg Szep <gregory.szep@gmail.com>
2018-03-12 16:21:36 +0000

    writing pixels to server disk

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ Module initialising server with site
 RESET = '\x1b[0m'
 CYAN = '\x1b[36m'
 
+function uuid() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+		return v.toString(16)
+	})
+}
+
 // serve app with views and static files
 console.log(CYAN+'[Node]'+RESET+' Configuring Application')
 const WebSocket = require('ws')
@@ -25,9 +32,12 @@ socket.on('connection', socket => {
 	socket.send('[Node] Data transfer socket connected')
 	console.log('\n'+CYAN+'[Node]'+RESET+' Client connected',socket._socket.remoteAddress.replace(/^.*:/, ''))
 
-	socket.on('message', message => {
-		console.log(CYAN+'[Node]'+RESET,message)
-		fs.appendFileSync('array.txt', message)
+	socket.on('message', buffer => {
+		let id = uuid()
+
+		fs.write('jupyter/notebooks/data/'+id,buffer, () => {
+			console.log(CYAN+'[Node]'+RESET+' set data '+id)
+		})
 	})
 })
 
